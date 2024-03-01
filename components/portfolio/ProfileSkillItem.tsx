a59fcd458e13882f1ae20fcc64c3df7c412d8588
Francisco <fra.zelada.s@gmail.com>
2024-03-01 12:31:42 -0300

    shadows fix

--- components/portfolio/ProfileSkillItem.tsx
+++ components/portfolio/ProfileSkillItem.tsx
@@ -18,7 +18,11 @@ export const ProfileSkillItem: FC<Props> = ({ img }) => {
 				objectFit: 'contain',
 			}}>
 			<img
-				style={{ maxHeight: '65px', maxWidth: '65px' }}
+				style={{
+					maxHeight: '65px',
+					maxWidth: '65px',
+					filter: 'drop-shadow(0 0.2rem 0.25rem rgba(150, 150, 150, 1))',
+				}}
 				height={'65px'}
 				width={'65px'}
 				src={img}
